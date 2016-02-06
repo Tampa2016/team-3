434ba5ada39a214f310af8244a2f4c507dd1e530
Nicholas Miller <nickbryanmiller@gmail.com>
2016-02-06 03:33:19 -0500

    Added photos

--- iOS App/Code_For_Good/PhotoViewController.swift
+++ iOS App/Code_For_Good/PhotoViewController.swift
@@ -8,28 +8,77 @@
 
 import UIKit
 
-class PhotoViewController: UIViewController {
-
+class PhotoViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    let imagePicker = UIImagePickerController()
+    
+    @IBOutlet weak var cameraButton: UIButton!
+    
+    @IBOutlet weak var myImageView: UIImageView!
+    
+    @IBOutlet weak var cancelButton: UIButton!
+    @IBOutlet weak var acceptButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        imagePicker.delegate = self
+        
+        if (!UIImagePickerController.isSourceTypeAvailable(.Camera)){
+            cameraButton.enabled = false
+        }
+        
+        if (myImageView.image == nil) {
+            cancelButton.hidden = true
+            acceptButton.hidden = true
+        }
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    @IBAction func cancelButtonTouched(sender: AnyObject) {
+        myImageView.image = nil
+        cancelButton.hidden = true
+        acceptButton.hidden = true
     }
-    */
-
+    
+    @IBAction func cameraButtonTouched(sender: AnyObject) {
+        imagePicker.allowsEditing = false
+        imagePicker.sourceType = .Camera
+        
+        presentViewController(imagePicker, animated: true, completion: nil)
+    }
+    
+    @IBAction func albumButtonPicked(sender: AnyObject) {
+        imagePicker.allowsEditing = false
+        imagePicker.sourceType = .PhotoLibrary
+        
+        presentViewController(imagePicker, animated: true, completion: nil)
+    }
+    
+    func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : AnyObject]) {
+        if let pickedImage = info[UIImagePickerControllerOriginalImage] as? UIImage {
+            myImageView.contentMode = .ScaleToFill
+            myImageView.image = pickedImage
+            
+            print(pickedImage)
+        }
+        
+        cancelButton.hidden = false
+        acceptButton.hidden = false
+        dismissViewControllerAnimated(true, completion: nil)
+        
+    }
+    
+    //    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+    //        if segue.identifier == "PhotoToInfo" {
+    //            let infoViewController = segue.destinationViewController as! InfoViewController
+    //            print(myImageView.image)
+    //            infoViewController.myImageView.image = myImageView.image
+    //        }
+    //    }
+    
 }
