a2c2a936fb9236f7bf31b3b9e295aee749687f1d
Nicholas Miller <nickbryanmiller@gmail.com>
2016-02-06 01:29:27 -0500

    Integrated Nick and Kristie's branch together

--- iOS App/Code_For_Good/ExploreResultsViewController.swift
+++ iOS App/Code_For_Good/ExploreResultsViewController.swift
@@ -8,12 +8,16 @@
 
 import UIKit
 
-class ExploreResultsViewController: UIViewController {
+class ExploreResultsViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
+    
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        tableView.dataSource = self
+        tableView.delegate = self
     }
 
     override func didReceiveMemoryWarning() {
@@ -21,15 +25,15 @@ class ExploreResultsViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 20
     }
-    */
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("ExploreResultsTableViewCell", forIndexPath: indexPath) as! ExploreResultsTableViewCell
+        
+        
+        return cell
+    }
 
 }
